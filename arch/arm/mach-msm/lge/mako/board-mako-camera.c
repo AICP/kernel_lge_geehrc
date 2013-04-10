4749bf68c84011a97de06e45ce5e3c61f68e3081
Seth Shelnutt <shelnutt2@gmail.com>
2013-04-10 18:48:54 -0400

    Remove imx091 from ifdef for voltage correction
    Change-Id: I6e979c412d98c5e7d2eedf0851a36ad2cc8ac461

--- arch/arm/mach-msm/lge/mako/board-mako-camera.c
+++ arch/arm/mach-msm/lge/mako/board-mako-camera.c
@@ -344,7 +344,7 @@ static struct camera_vreg_t apq_8064_back_cam_vreg[] = {
 	{"cam1_vdig", REG_LDO, 1200000, 1200000, 105000},
 	{"cam1_vio", REG_VS, 0, 0, 0},
 	{"cam1_vana", REG_LDO, 2850000, 2850000, 85600},
-#if defined(CONFIG_IMX111) || defined(CONFIG_IMX091)
+#if defined(CONFIG_IMX111)
 	{"cam1_vaf", REG_LDO, 2800000, 2800000, 300000},
 #else
 	{"cam1_vaf", REG_LDO, 1800000, 1800000, 150000},
